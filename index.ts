55dc967ffd7019aa820958f5385a6490e8f64759
Aliowais8514 <160965769+Aliowais8514@users.noreply.github.com>
2024-04-15 04:00:49 +0500

    push my atm machine project

---
+++ index.ts
@@ -0,0 +1,54 @@
+import inquirer from "inquirer";
+//intialize user balance and pin
+let mybalance = 5000;
+let mypin = 1234;
+
+//print welcom message
+console.log("welcome to code with owais - ATM machine");
+
+let pinAnswer = await inquirer.prompt([
+    {
+         name: "pin",
+         type: "number",
+         message: "ENTER YOUR PIN CODE:"
+        }
+    ])
+    if (pinAnswer.pin === mypin ){
+        console.log("pin is correct login sucessfully!");
+       // console.log("current account balance is ${my balance}")
+        
+        let operationAns = await inquirer.prompt([
+            {
+                name: "operation",
+                type: "list",
+                message: "select an operation",
+                choices: ["withdrawamount", "check balance"]
+            }]);
+
+            console.log(operationAns);
+
+            if(operationAns.operation === "withdrawamount"){
+                let amountAns = await inquirer.prompt([
+                    {
+                        name: "amount",
+                        message: "enter your amount",
+                        type: "number",
+                        
+                    }
+                ])
+                if(amountAns.amount > mybalance){
+                    console.log("insufficient balance")
+                }
+                else{
+                    mybalance -= amountAns.amount,
+                    console.log(`${amountAns.amount} withdrawl sucessfully`);
+                    console.log(`your remainig balance is: ${mybalance}`);
+                }
+                }
+                else if (operationAns.operation === "check balance"){
+                    console.log(`your account balance is: ${mybalance}`);
+                }
+            }
+    else{
+        console.log("pin is incorrect, try again!");
+    }
